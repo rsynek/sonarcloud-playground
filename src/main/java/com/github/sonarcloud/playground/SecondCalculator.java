c9a16012bd93068ddd5ed4a9b2d6b263156d80fb
Radovan Synek <rsynek@redhat.com>
2019-01-02 21:35:50 +0100

    Add second calculator

---
+++ src/main/java/com/github/sonarcloud/playground/SecondCalculator.java
@@ -0,0 +1,20 @@
+package com.github.sonarcloud.playground;
+
+public class SecondCalculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
