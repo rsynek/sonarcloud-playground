d97592f990a74dd7409a0ae6282c80c46c710467
Radovan Synek <rsynek@redhat.com>
2019-01-03 14:21:23 +0100

    Use external script to invoke sonar analysis

---
+++ travis-sonarcloud.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]
+then
+  echo "PR"
+  mvn --quiet jacoco:report jacoco:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=rsynek-github -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=rsynek_sonarcloud-playground -Dsonar.pullrequest.base=$TRAVIS_BRANCH -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.github.repository=$TRAVIS_PULL_REQUEST_SLUG
+else
+  echo "not PR"
+  mvn --quiet jacoco:report jacoco:merge sonar:sonar -Preport-code-coverage -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=rsynek-github -Dsonar.login=$SONARCLOUD_TOKEN -Dsonar.projectKey=rsynek_sonarcloud-playground
+fi
